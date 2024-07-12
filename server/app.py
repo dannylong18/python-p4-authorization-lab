9cfe18a4fbc2b042f4475ee18f5de442507ea56d
Daniel Long <danielwlong618@gmail.com>
2024-07-11 22:26:22 -0500

    lab done

--- server/app.py
+++ server/app.py
@@ -18,6 +18,12 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.before_request
+def check_if_logged_in():
+    if not session.get('user_id') and (request.endpoint in ['member_index', 'member_article']):
+        return {'error': 'Unauthorized'}, 401
+
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -87,12 +93,19 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        articles = Article.query.filter_by(is_member_only=True).all()
+        articles_json = [article.to_dict() for article in articles]
+        return make_response(jsonify(articles_json), 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id, Article.is_member_only == True).first()
+        
+        if article is None:
+            return jsonify({'error': 'Article not found or not accessible'}), 404
+
+        return jsonify(article.to_dict())
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
